20d4bebaf48fc69c7b9fa7d95439334c51748aaf
aguilar2005 <148366555+aguilar2005@users.noreply.github.com>
2025-05-09 14:35:44 +0800

    updated

--- index.js
+++ index.js
@@ -13,78 +13,102 @@ const scroll = new LocomotiveScroll({
 
 // PROJECT SECTION //
 const projectsDetails = [
-  {name: "International Rice Research Incorporation",
+  {
+    id: 1,
+    name: "International Rice Research Incorporation",
     year: 2015,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
-    imgSource: "project-01.jpg"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doore magna aliqua. Ut enum ad minim veriam, quis nostrud exercitaion ullamco laboris nidi ut alliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate celit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt moolit anim id est laborum.",
+    overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    imgSource: "project-01.jpg",
+    coordinates: [14.1788, 121.2408],
+    address: "Batong Malake Los Baños Laguna 4030"
   },
-  {name: "Olivarez Plaza Los Baños",
+  {
+    id: 2,
+    name: "Olivarez Plaza Los Baños",
     year: 2017,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
-    imgSource: "project-02.jpg"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doore magna aliqua. Ut enum ad minim veriam, quis nostrud exercitaion ullamco laboris nidi ut alliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate celit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt moolit anim id est laborum.",
+    overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    imgSource: "project-02.jpg",
+    coordinates: [14.3197, 120.7683],
+    address: "Batong Malake Los Baños Laguna 4030"
   },
-  {name: "Los Baños Laguna National Hospital",
+  {
+    id: 3,
+    name: "Los Baños Doctors Hospital and Medical Center (LBDHMC)",
     year: 2021,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
-    imgSource: "project-03.webp"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doore magna aliqua. Ut enum ad minim veriam, quis nostrud exercitaion ullamco laboris nidi ut alliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate celit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt moolit anim id est laborum.",
+    overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    imgSource: "project-03.webp",
+    coordinates: [14.1650, 121.2435],
+    address: "Batong Malake Los Baños Laguna 4030"
   },
 ]
 const projectContainer = document.querySelector('.projects')
 
-projectsDetails.forEach(project => {
-  projectContainer.innerHTML += `
-    <div class="project">
-        <div class="img-container">
-            <img src="images/${project.imgSource}" alt="">
-        </div>
-        <div class="content">
-            <span>${project.name}</span>
-            <p class="year">Year ${project.year}</p>
-            <p>${project.description}</p>
-            <a href="">view more</a>
-        </div>
-    </div>
-  `
-})
+if(projectContainer){
+  projectsDetails.forEach(project => {
+    projectContainer.innerHTML += `
+      <div class="project">
+          <div class="img-container">
+              <img src="images/${project.imgSource}" alt="">
+          </div>
+          <div class="content">
+              <span>${project.name}</span>
+              <p class="year">Year ${project.year}</p>
+              <p>${project.overview}</p>
+              <a href="projects/view-projects.html?id=${project.id}">view more</a>
+          </div>
+      </div>
+    `
+  })
+}
 
 
 
 // SCRIPT FOR MAP //
-let isTwoFingerTouch = false
 
+const dragButtonCheckbox = document.querySelector('#drag-map')
+const dragButtonCheckboxLabel = document.querySelector('#drag-map-label')
+const resetMapViewButton = document.querySelector('#reset')
 
-const map = L.map('map', {
-  center: [14.17025, 121.24181], // Los Baños
-  zoom: 14,
-  zoomControl: true,
-  scrollWheelZoom: false,
-  doubleClickZoom: false,
-  touchZoom: false,
-  dragging: false
-});
+if(document.querySelector('.home-map')){
+  // Create the map once
+  const map = L.map('map', {
+    center: [14.17025, 121.24181], // Los Baños
+    zoom: 14,
+    zoomControl: true,
+    scrollWheelZoom: false,
+    doubleClickZoom: false,
+    touchZoom: false,
+    dragging: false // Initially off
+  })
+  
+  // Add tile layer and marker
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; OpenStreetMap & CartoDB',
+    subdomains: 'abcd',
+    maxZoom: 19
+  }).addTo(map)
+  
+  L.marker([14.17025, 121.24181]).addTo(map)
+    .bindPopup(`<b>Hello!</b><br>This is Los Baños Laguna.`)
+    .openPopup()
+  
+  // Toggle drag dynamically
+  dragButtonCheckbox.addEventListener('change', () => {
+    if (dragButtonCheckbox.checked) {
+      dragButtonCheckboxLabel.textContent = "drag enabled"
+      map.dragging.enable()
+    } else {
+      dragButtonCheckboxLabel.textContent = "drag disabled"
+      map.dragging.disable()
+    }
+  })
+}
 
-// Clean tile style
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; OpenStreetMap & CartoDB',
-  subdomains: 'abcd',
-  maxZoom: 19
-}).addTo(map);
-
-
-const marker = L.marker([14.17025, 121.24181]).addTo(map)
-  .bindPopup(`<b>Hello!</b><br>This is Los Baños Laguna.`)
-  .openPopup();
-
-map.on('touchstart', (e) => {
-  if(e.originalEvent.touches.length === 2){
-    isTwoFingerTouch = true
-    map.dragging.enable()
-  } else {
-    isTwoFingerTouch = false
-    map.dragging.disable()
-  }
-})
-
-map.on('touchend', () => {
-  map.dragging.disable()
-})
+if(resetMapViewButton){
+  resetMapViewButton.addEventListener('click', () => {
+    map.setView([14.17025, 121.24181], 14);
+  })
+}
---
+++ projects/view-projects.js
@@ -0,0 +1,62 @@
+const projectNameText = document.querySelector('.project-name')
+const addressText = document.querySelector('.address')
+const yearText = document.querySelector('.year')
+const coordinatesText = document.querySelector('.coordinates')
+const imageElement = document.querySelector('img.image')
+const descriptionText = document.querySelector('.descriptionText')
+
+const searchParam = new URLSearchParams(window.location.search)
+const id = searchParam.get('id')
+
+
+projectsDetails.forEach(project => {
+
+    if(project.id == id){
+        projectNameText.textContent = project.name
+        addressText.textContent = project.address
+        yearText.textContent = project.year
+        coordinatesText.textContent = project.coordinates
+        imageElement.src = `../images/${project.imgSource}`
+        descriptionText.textContent = project.description
+        
+        const map = L.map('map', {
+            center: project.coordinates, // Los Baños
+            zoom: 14,
+            zoomControl: true,
+            scrollWheelZoom: false,
+            doubleClickZoom: false,
+            touchZoom: false,
+            dragging: false // Initially off
+          })
+          
+          // Add tile layer and marker
+          L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; OpenStreetMap & CartoDB',
+            subdomains: 'abcd',
+            maxZoom: 19
+          }).addTo(map)
+          
+          L.marker(project.coordinates).addTo(map)
+            .bindPopup(`<b>Hello!</b><br>This is ${project.name}.`)
+            .openPopup()
+    }
+
+})
+
+
+const otherProjectsContainer = document.querySelector('.other-projects-container')
+projectsDetails.forEach(project =>{
+    otherProjectsContainer.innerHTML += `
+        <div class="project">
+            <div class="img-container">
+                <img src="../images/${project.imgSource}" alt="">
+            </div>
+            <div class="content">
+                <span>${project.name}</span>
+                <p class="year">Year ${project.year}</p>
+                <p>${project.overview}</p>
+                <a href="view-projects.html?id=${project.id}">view more</a>
+            </div>
+        </div>
+    `
+})
